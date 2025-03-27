d78f195f2c74514a09ab3307b2a7876eb1228bcc
bloodsweatncode <christian.schmidt@bluedragon.de>
2025-03-27 19:58:42 +0100

    Added LeadNoe Class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -13,8 +13,20 @@ class HTMLNode():
 
     def props_to_html(self):
         html = ""
+        if self.props == None:
+            return html
         keys = list(self.props.keys())
         values = list(self.props.values())
         for i in range(0, len(keys)):
             html += f" {keys[i]}=\"{values[i]}\"" 
         return html
+    
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag = None, value = None, props = None):
+        if not value:
+            raise ValueError("LeafNode must have a value")
+        super().__init__(tag, value, [], props)
+
+    def to_html(self):
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -10,8 +10,20 @@ class TestHTMLNode(unittest.TestCase):
         
     def test_eq_false(self):
         node = HTMLNode("div", "This is a text node", [], {"a": "la", "b": "lb"})
-        self.assertNotEqual(node.props_to_html(), "\{'a': 'la', 'b': 'lb'\}")
+        self.assertNotEqual(node.props_to_html(), "{'a': 'la', 'b': 'lb'}")
         
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_b(self):
+        node = LeafNode("b", "Hello, world!")
+        self.assertEqual(node.to_html(), "<b>Hello, world!</b>")
+
+    def test_leaf_to_html_i(self):
+        node = LeafNode("i", "Hello, world!")
+        self.assertEqual(node.to_html(), "<i>Hello, world!</i>")
+
 
 
 if __name__ == "__main__":
