3fc25be7f238d8f3563dfdc5cfe50f26264f0fa3
bloodsweatncode <christian.schmidt@bluedragon.de>
2025-04-21 16:14:01 +0200

    Added this and that

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -2,7 +2,6 @@ import unittest
 
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from textnode import TextNode, TextType
-from text_to_html import text_node_to_html_node
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -39,12 +38,6 @@ class TestHTMLNode(unittest.TestCase):
             parent_node.to_html(),
             "<div><span><b>grandchild</b></span></div>",
         )
-
-    def test_text(self):
-        node = TextNode("This is a text node", TextType.TEXT)
-        html_node = text_node_to_html_node(node)
-        self.assertEqual(html_node.tag, None)
-        self.assertEqual(html_node.value, "This is a text node")
 
 
 if __name__ == "__main__":
